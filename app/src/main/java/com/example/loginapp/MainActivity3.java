30507fb6873101985b62dfeb51978973f6eca534
a.zulfanur <2ez4second@gmail.com>
2021-10-13 08:58:32 +0700

    2nd Commit

--- app/src/main/java/com/example/loginapp/MainActivity3.java
+++ app/src/main/java/com/example/loginapp/MainActivity3.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    private ImageButton btn_masuk;
+    private Button btn_masuk;
+    private ImageButton btn_login;
+    private EditText emailbang;
+    private EditText pwBang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +23,30 @@ public class MainActivity3 extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
 
         btn_masuk = findViewById(R.id.btn_masuk);
+        btn_login = findViewById(R.id.btn_login);
+        emailbang = findViewById(R.id.emailbang);
+        pwBang = findViewById(R.id.pwBang);
 
         btn_masuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent ganti = new Intent(MainActivity3.this, MainActivity4.class);
                 startActivity(ganti);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (emailbang.getText().toString().equals("azulfanur") && pwBang.getText().toString().equals("root")){
+                    //Correct
+                    Toast.makeText(MainActivity3.this,"Login Succses",Toast.LENGTH_SHORT).show();
+                    Intent login = new Intent(MainActivity3.this, MainActivity5.class);
+                    startActivity(login);
+                }else {
+                    //Incorrect
+                    Toast.makeText(MainActivity3.this,"Password Incorrect",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
